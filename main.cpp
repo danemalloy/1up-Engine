7444fd1731c35341c21d027212f34c7eb2a486b7
danemalloy <damalloy2@gmail.com>
2025-02-27 17:50:12 -0800

    added external folder within project added imgui to external setup imgui includes

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// windows api includes
+#include <Windows.h>
+
+/// imgui includes
+#include <imgui.h>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx11.h>
+#include <d3d9.h>
+#include <tchar.h>
+
+int main()
+{
+  std::cout << "Hello, World!" << "\n";
+
+  std::cin.get();
+
+  return 0;
+}
