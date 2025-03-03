580c81eae09bafd909cd5c5e2a9f65bb50608f5a
danemalloy <damalloy2@gmail.com>
2025-03-02 20:42:25 -0800

    added reading and writing memory

--- main.cpp
+++ main.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
-
 // windows api includes
 #include <Windows.h>
 
-// any process that has PROCESS_VM_READ access can call this function
-/// if the area to read is not accessible, the function will fail
-BOOL ReadProcessMemory(
-  [in] HANDLE hProcess,               // handle to the process whose memory we will be reading
-  [in] LPCVOID lpBaseAddress,         // pointer to the base address in the process that we will read from
-  [out] LPVOID lpBuffer,              // pointer to the buffer that will receive the data read from the process
-  [in] SIZE_T nSize,                  // number of bytes to read from the process
-  [out] SIZE_T* lpNumberOfBytesRead   // pointer to the variable that receives number of bytes read from the process
-)
+int main()
 {
+  // find and open game window
+  HWND game_window = FindWindow(NULL, L"The Battle for Wesnoth - 1.18.4");
 
-}
+  if (!game_window) {
+    std::cerr << "game window not found" << "\n";
+    return 1;
+  }
 
-int main(int argc, char** argv)
-{
-  DWORD changeValue{ 0 };
-  DWORD bytesRead{ 0 };
+  DWORD process_id{ 0 };
+  GetWindowThreadProcessId(game_window, &process_id);
+
+  HANDLE game_process{ OpenProcess(PROCESS_ALL_ACCESS, true, process_id) };
+
+  if (!game_process) {
+    std::cerr << "failed to open process | error code: " << GetLastError() << "\n";
+    return 1;
+  }
+
+  uintptr_t input_address{ 0 };
+
+  std::cout << "enter memory address: ";
+  std::cin >> std::hex >> input_address;
+
+  void* memory_address{ (void*)input_address };
+  DWORD memory_value{ 0 };
+
+  unsigned long long bytes_read{ 0 };
+
+  if (!ReadProcessMemory(game_process, (LPCVOID)memory_address, &memory_value, sizeof(memory_value), &bytes_read))
+  {
+    std::cerr << "failed to read value of process memory | error code: " << GetLastError() << "\n";
+    return 1;
+  }
+
+  unsigned int input_write_value{ 0 };
+  unsigned long long bytes_written{ 0 };
+
+  std::cout << "enter value to write: ";
+  std::cin >> std::dec >> input_write_value;
+
+  DWORD write_value{ input_write_value };
+
+  std::cout << "writing value " << write_value << " to memory address..." << "\n";
+
+  if (!WriteProcessMemory(game_process, (LPVOID)memory_address, &write_value, sizeof(write_value), &bytes_written))
+  {
+    std::cerr << "failed to write value to process memory | error code: " << GetLastError() << "\n";
+    return 1;
+  }
+
+  std::cout << "address: " << memory_address << "\n" << "previous value: " << memory_value << "\n" << "new value: " << write_value << "\n";
+
+  std::cin.ignore();
+  std::cin.get();
 
   return 0;
 }
